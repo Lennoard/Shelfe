732a055639f9e76c094b7a850eab2d83d58e36b5
Lennoard <lennoardrai@gmail.com>
2021-10-07 23:03:04 -0300

    feature: [WIP] início tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './styles/global.css';
+import Routes from "./routes";
+import { createTheme, ThemeProvider } from '@material-ui/core';
 
-import Routes from './routes';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2F9FA1",
+      light: "#69D0D2",
+      dark: "#007073",
+    },
+  },
+});
 
 function App() {
   return (
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
   );
 }
 
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import { Button, Container, TextField, Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+import GoogleSignInButton from "../components/GoogleSignInButton";
+
+import bookLogo from "../images/pages/signin/book.svg";
+
+import "../styles/pages/signin.css";
 
 function SignIn() {
-  return (<div></div>);
+  return (
+    <Container fixed>
+      <Grid container rowSpacing={1} columnSpacing={4}>
+        <Grid id="left" item xs={6}>
+          <img id="shelfe-logo" src={bookLogo} alt="Shelfe logo" />
+          <Typography marginTop="24px" variant="h3">
+            Shelfe
+          </Typography>
+          <Typography marginTop="16px" variant="body1">
+            Gerenciador de progresso de leitura e resenhas de livros
+          </Typography>
+        </Grid>
+        <Grid id="right" item xs={6}>
+          <TextField
+            id="email"
+            type="email"
+            label="Email"
+            margin="dense"
+            required
+          />
+          <TextField
+            id="password"
+            type="password"
+            label="Senha"
+            margin="dense"
+            required
+          />
+
+          <div>
+            <Button id="sign-in-button" variant="outlined">Entrar com email</Button>
+            <GoogleSignInButton />
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default SignIn;
